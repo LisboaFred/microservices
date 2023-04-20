0d897b5a22bed9baad2709e0093ea233dfd1b6d5
LisboaFred <contato.fredericolisboa@gmail.com>
2023-04-19 23:42:56 -0300

    Feat: produto controller and usercase

--- produto/src/server.ts
+++ produto/src/server.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
+import { router } from "./routes";
 
 const PORT = process.env.PORT ?? 3003;
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () =>
   console.log(`Server produto is running on PORT ${PORT}`)
